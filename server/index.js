100a1094adc595b4e3cda06f08e18654fceb2099
tangxq <tangxiaoqiang@chinawbb.com>
2018-11-08 17:03:38 +0800

    feat: 增加模板引擎中间件koa-views

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const Koa = require('koa')
 const app = new Koa()
-const { htmlTpl,ejsTpl,pugTpl } = require('./tpl')
+// const { htmlTpl,ejsTpl,pugTpl } = require('./tpl')
 // const ejs = require('ejs')
-const pug = require('pug')
+// const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+} ))
+
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf-8'
-  ctx.body = pug.render(pugTpl,{
-    you: "xxx",
-    me: 'tian yan rong'
+  await ctx.render('index', {
+    you:'tianyanrong',
+    me: 'tangxiaoqiang'
   })
 })
 app.listen(4455)
